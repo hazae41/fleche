a190ac5ccd6dc87286989a639f77e1c98b1e6bae
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-16 14:35:40 +0200

    fix

--- tools/deno/ws_client.ts
+++ tools/deno/ws_client.ts
@@ -1,17 +1,22 @@
 const socket = new WebSocket("ws://localhost:8080")
 
+socket.binaryType = "arraybuffer"
+
 await new Promise<void>((ok, err) => {
   socket.addEventListener("open", () => ok())
   socket.addEventListener("error", () => err())
   socket.addEventListener("close", () => err())
 })
 
-socket.addEventListener("message", e => {
-  const buffer = e.data as ArrayBuffer
-  console.debug(new Uint8Array(buffer))
+socket.send(new Uint8Array([1, 2, 3]))
+
+const reply = await new Promise<Uint8Array>((ok, err) => {
+  socket.addEventListener("message", e => ok(new Uint8Array(e.data)))
+  socket.addEventListener("error", () => err())
+  socket.addEventListener("close", () => err())
 })
 
-socket.send(new Uint8Array([1, 2, 3]))
+console.debug(reply)
 
 socket.close(3123)
 
