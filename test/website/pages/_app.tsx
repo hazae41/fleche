6f648b5f323a5fc4866906b6963f1d814e2de0ab
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2023-05-22 15:50:04 +0200

    debug

--- test/website/pages/_app.tsx
+++ test/website/pages/_app.tsx
@@ -1,5 +1,8 @@
-import '../styles/globals.css'
+import { Debug } from '@hazae41/result'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+
+Debug.debug = true
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
